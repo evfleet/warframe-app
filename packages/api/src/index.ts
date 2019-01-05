f9e573fb91a40d27815e3a9215ea7391fe59ab18
Evan Fleet <evfleet@gmail.com>
2019-01-04 21:55:36 -0500

    tweak prettier and tsconfig

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,16 @@
 import "reflect-metadata";
+import express from "express";
 import { createConnection } from "typeorm";
 
 createConnection()
-  .then(async connection => {
-    console.log("Here you can setup and run express/koa/any other framework.");
+  .then(async (connection) => {
+    const port = process.env.port || 3000;
+    const server = express();
+
+    server.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
   })
-  .catch(error => console.log(error));
+  .catch((error) => {
+    console.log(`Something went wrong: ${error}`);
+  });
