031810fd0b354d529b5a2e9bc69084be197058c8
Evan Fleet <evfleet@gmail.com>
2019-01-05 18:06:16 -0500

    setup basic docker for api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,27 @@
 import "reflect-metadata";
 import express from "express";
-import { createConnection } from "typeorm";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+// import { createConnection } from "typeorm";
 
+const port = process.env.PORT;
+const server = express();
+
+server.use(helmet());
+server.use(bodyParser.json());
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
+/*
 createConnection()
   .then(async (connection) => {
-    const port = process.env.port || 3000;
+    const port = process.env.port;
     const server = express();
+
+    server.use(helmet());
+    server.use(bodyParser.json());
 
     server.listen(port, () => {
       console.log(`Server running on port: ${port}`);
@@ -14,3 +30,4 @@ createConnection()
   .catch((error) => {
     console.log(`Something went wrong: ${error}`);
   });
+*/
